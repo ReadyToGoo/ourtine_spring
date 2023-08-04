61405b3bd4e39e77f387eb085e8f473269b1f23b
HyoBin Yang <50162252+HyoBN@users.noreply.github.com>
2023-08-04 20:44:55 +0900

    docs: API Swagger 설명 추가 (#51)
    * fix: Beanstalk check 시간 조정
    
    * feat: User Profile image 변경 기능 추가
    
    * feat: 유저,습관 프로필, 습관인증영상 업로드 구현
    
    * feat: 회원가입, 습관 생성 시 이미지 업로드 기능 추가
    
    * docs: API Swagger 설명 추가

--- src/main/java/ourtine/web/controller/HabitController.java
+++ src/main/java/ourtine/web/controller/HabitController.java
@@ -53,6 +53,7 @@ public class HabitController {
 
     // 습관 프로필 사진 업로드
     @PatchMapping(value="/habit/{id}/profile",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ApiOperation(value = "습관 프로필 사진 변경",notes="습관의 프로필 사진을 변경한다.")
     public void updateHabitProfileImage(@PathVariable Long id, HttpServletRequest request, @RequestParam(value="image") MultipartFile image) throws IOException {
         // 자신이 생성한 Habit인지 체크해야 함.
         Habit habit = habitService.findById(id);

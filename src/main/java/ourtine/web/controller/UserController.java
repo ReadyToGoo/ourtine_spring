00992c5be0d9ea9ebc074cd9a1ac7b4e8d26efbb
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2023-08-05 13:32:09 +0900

    fix : 충돌 코드 수정

--- src/main/java/ourtine/web/controller/UserController.java
+++ src/main/java/ourtine/web/controller/UserController.java
@@ -1,5 +1,6 @@
 package ourtine.web.controller;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,17 +21,29 @@ public class UserController {
     private final UserService userService;
     private final UploadService uploadService;
 
+    // 프로필 사진 업로드 추가를 위해 임시로 만든 회원가입 API
+    @PostMapping("/user/signup")
+    @ApiOperation(value = "(미완)회원 가입",notes="(미완)회원 가입을 한다.")
+    public ResponseEntity signUp(@RequestParam(value="image") MultipartFile image) throws IOException {
+        User user = new User();
+        user.updateImage(uploadService.uploadUserProfile(image));
+        userService.saveOrUpdateUser(user);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
     @PostMapping("/user/{id}/nickname")
+    @ApiOperation(value = "닉네임 변경",notes="User의 닉네임을 변경한다.")
     public ResponseEntity ChangeNickname(@PathVariable Long id, @RequestBody @Valid NicknameChangeRequestDto nicknameChangeRequestDto){//형식에 맞게 수정 필요
         userService.changeNickname(id, nicknameChangeRequestDto.getNickname());
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @ResponseBody   // Long 타입을 리턴하고 싶은 경우 붙여야 함 (Long - 객체)
     @PatchMapping(value="/user/{id}/profile",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void uploadUserProfile(@PathVariable Long id,  @RequestParam(value="image") MultipartFile image) throws IOException {
+    @ApiOperation(value = "유저 프로필 사진 변경",notes="유저의 프로필 사진을 변경한다.")
+    public ResponseEntity updateUserProfileImage(@PathVariable Long id,  @RequestParam(value="image") MultipartFile image) throws IOException {
         User user = userService.findById(id);
         user.updateImage(uploadService.uploadUserProfile(image));
         userService.saveOrUpdateUser(user);
+        return new ResponseEntity(HttpStatus.OK);
     }
 }

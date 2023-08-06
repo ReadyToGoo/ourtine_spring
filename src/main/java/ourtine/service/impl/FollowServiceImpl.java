1e0a6c9cbf880b58b74a4257704747045743e271
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2023-08-07 01:21:38 +0900

    feat : follow service 로직 수정

--- src/main/java/ourtine/service/impl/FollowServiceImpl.java
+++ src/main/java/ourtine/service/impl/FollowServiceImpl.java
@@ -3,10 +3,13 @@ package ourtine.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ourtine.domain.Follow;
 import ourtine.domain.User;
+import ourtine.exception.BusinessException;
+import ourtine.exception.enums.ResponseMessage;
 import ourtine.repository.FollowRepository;
 import ourtine.repository.UserRepository;
 import ourtine.service.FollowService;
@@ -26,7 +29,7 @@ public class FollowServiceImpl implements FollowService {
     @Override
     public FollowGetResponseDto getFollowStatus(FollowGetRequestDto requestDto, User me) {
         if (userRepository.findById(requestDto.getUserId()).isPresent()){
-            if (followRepository.findBySenderAndReceiverId(me, requestDto.getUserId()).isPresent()) {
+            if (followRepository.findBySenderIdAndReceiverId(me.getId(), requestDto.getUserId()).isPresent()) {
                 return new FollowGetResponseDto(requestDto.getUserId(), true);
             }
             else  return new FollowGetResponseDto(requestDto.getUserId(), false);
@@ -36,10 +39,11 @@ public class FollowServiceImpl implements FollowService {
 
     // 팔로우 하기
     @Override
+    @Transactional
     public FollowPostResponseDto followUser(FollowPostRequestDto requestDto, User me) {
         User receiver = userRepository.findById(requestDto.getUserId()).orElseThrow();
         // 이미 팔로우하고 있으면
-        if (followRepository.findBySenderAndReceiverId(me, requestDto.getUserId()).isPresent()){
+        if (followRepository.findBySenderIdAndReceiverId(me.getId(), requestDto.getUserId()).isPresent()){
             // 에러 처리
         }
         else {
@@ -50,14 +54,17 @@ public class FollowServiceImpl implements FollowService {
 
     // 언팔로우 하기
     @Override
+    @Modifying
+    @Transactional
     public FollowDeleteResponseDto unfollowUser(FollowDeleteRequestDto requestDto, User me) {
-        User following = userRepository.findById(requestDto.getUserId()).orElseThrow();
+        userRepository.findById(requestDto.getUserId()).orElseThrow(
+                ()->new BusinessException(ResponseMessage.WRONG_USER));
         // 팔로우하고 있지 않으면
-        if (followRepository.findBySenderAndReceiverId(me, requestDto.getUserId()).isEmpty()){
+        if (followRepository.findBySenderIdAndReceiverId(me.getId(), requestDto.getUserId()).isEmpty()){
             // 에러 처리
         }
         else {
-            Follow follow = followRepository.findBySenderAndReceiverId(me, requestDto.getUserId()).get();
+            Follow follow = followRepository.findBySenderIdAndReceiverId(me.getId(), requestDto.getUserId()).get();
             followRepository.delete(follow);
         }
         return new FollowDeleteResponseDto(requestDto.getUserId());
@@ -66,7 +73,7 @@ public class FollowServiceImpl implements FollowService {
     // 내 팔로잉 목록
     @Override
     public Slice<FollowingsGetResponseDto> getMyFollowing(User me, Pageable pageable) {
-        Slice<Follow> result = followRepository.findBySenderOrderByCreatedAtDesc(me);
+        Slice<Follow> result = followRepository.findBySenderIdOrderByCreatedAtDesc(me.getId());
         Slice<FollowingsGetResponseDto> followings = result.map(following ->
                 new FollowingsGetResponseDto(following.getReceiver().getId(),following.getReceiver().getNickname(),
                         following.getReceiver().getImageUrl()));
@@ -76,7 +83,7 @@ public class FollowServiceImpl implements FollowService {
     // 내 팔로워 목록
     @Override
     public Slice<FollowersGetResponseDto> getMyFollower(User me, Pageable pageable) {
-        Slice<Follow> result = followRepository.findByReceiverOrderByCreatedAtDesc(me);
+        Slice<Follow> result = followRepository.findByReceiverIdOrderByCreatedAtDesc(me.getId());
         Slice<FollowersGetResponseDto> followers = result.map(following ->
                 new FollowersGetResponseDto(following.getSender().getId(),following.getSender().getNickname(),
                         following.getSender().getImageUrl()));
@@ -86,7 +93,7 @@ public class FollowServiceImpl implements FollowService {
     // 유저 팔로잉 목록
     @Override
     public Slice<FollowingsGetResponseDto> getFollowing(Long userId, User me, Pageable pageable) {
-        Slice<Follow> result = followRepository.findBySenderIdOrderByCreatedAtDesc(userId);
+        Slice<Follow> result = followRepository.queryFindBySenderIdOrderByCreatedAtDesc(userId, me.getId());
         Slice<FollowingsGetResponseDto> followings = result.map(following ->
             new FollowingsGetResponseDto(following.getReceiver().getId(),following.getReceiver().getNickname(),
                     following.getReceiver().getImageUrl()));
@@ -96,7 +103,7 @@ public class FollowServiceImpl implements FollowService {
     // 유저 팔로워 목록
     @Override
     public Slice<FollowersGetResponseDto> getFollower(Long userId, User me, Pageable pageable) {
-        Slice<Follow> result = followRepository.findByReceiverIdOrderByCreatedAtDesc(userId);
+        Slice<Follow> result = followRepository.queryFindByReceiverIdOrderByCreatedAtDesc(userId, me.getId());
         Slice<FollowersGetResponseDto> followers = result.map(following ->
                 new FollowersGetResponseDto(following.getSender().getId(),following.getSender().getNickname(),
                         following.getSender().getImageUrl()));
--- src/main/java/ourtine/web/dto/response/HabitSessionGetResponseDto.java
+++ src/main/java/ourtine/web/dto/response/HabitSessionGetResponseDto.java
@@ -1,5 +1,6 @@
 package ourtine.web.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,8 +22,9 @@ public class HabitSessionGetResponseDto {
 
     private String title;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "kk:mm:ss", timezone = "Asia/Seoul")
     private LocalTime startTime;
-
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "kk:mm:ss", timezone = "Asia/Seoul")
     private LocalTime endTime;
 
     private List<HabitSessionFollowerResponseDto> followers = new ArrayList<>();

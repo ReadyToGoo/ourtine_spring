56fed78fc6acfedfaf8724b55ae0b2457c2a3126
HyoBN <qls0786@naver.com>
2023-08-14 15:03:55 +0900

    feat: User 인증 정보 받아오는 코드 추가

--- src/main/java/ourtine/web/controller/MessageController.java
+++ src/main/java/ourtine/web/controller/MessageController.java
@@ -4,10 +4,12 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import ourtine.domain.User;
+import ourtine.domain.UserDetailsImpl;
 import ourtine.service.MessageService;
 import ourtine.service.UserService;
 import ourtine.web.dto.common.BaseResponseDto;
@@ -20,13 +22,13 @@ public class MessageController {
     private final UserService userService;
     private final MessageService messageService;
 
-    @GetMapping("message/{userId}")
+    @GetMapping("message")
     @ApiOperation(value = "유저 메시지 조회", notes = "유저에게 온 메시지를 조회한다.")
-
-    public BaseResponseDto<SliceResponseDto<MessageResponseDto>> getMessages(@PathVariable Long userId, Pageable pageable) {
-        User user = userService.findById(userId);
+    public BaseResponseDto<SliceResponseDto<MessageResponseDto>> getMessages(Pageable pageable) {
+        UserDetailsImpl userDetails = (UserDetailsImpl) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userDetails.getUser();
+        Long userId = user.getId();
         Slice<MessageResponseDto> messages = messageService.getMessages(user, pageable);
-
         return new BaseResponseDto<>(new SliceResponseDto<>(messages));
     }
 }

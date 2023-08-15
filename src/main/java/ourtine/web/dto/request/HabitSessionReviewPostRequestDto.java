e10ecfd98866acc4a232547a8d95765165d44f11
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2023-08-15 21:51:54 +0900

    fix : dto 정규식 수정

--- src/main/java/ourtine/web/dto/request/HabitSessionReviewPostRequestDto.java
+++ src/main/java/ourtine/web/dto/request/HabitSessionReviewPostRequestDto.java
@@ -12,14 +12,13 @@ import javax.validation.constraints.*;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@ApiModel
 public class HabitSessionReviewPostRequestDto {
 
     @NotNull(message = "만족도 입력은 필수입니다.")
-    @Positive(message = "양수만 입력가능합니다.")
-    @Min(value = 1, message = "1 이상 5 이하의 정수만 입력 가능합니다.")
-    @Max(value = 5, message = "1 이상 5 이하의 정수만 입력 가능합니다.")
-    @Schema(title = "만족도", description = "1 이상 5 이하의 정수만 입력 가능합니다.")
+    @Positive(message = "자연수만 입력가능합니다.")
+    @Min(value = 1, message = "1 이상 5 이하의 자연수만 입력 가능합니다.")
+    @Max(value = 5, message = "1 이상 5 이하의 자연수만 입력 가능합니다.")
+    @Schema(title = "만족도", description = "1 이상 5 이하의 자연수만 입력 가능합니다.")
     private Long starRate;
 
     @NotNull(message = "감정 입력은 필수입니다")

8cda4af91fcc4965e5a2f9be20933da8f155d00f
HyoBin Yang <50162252+HyoBN@users.noreply.github.com>
2023-08-17 18:54:47 +0900

    feat: User WeeklyLog 작성 API 추가 (#130)
    * fix: 유저 프로필 사진 업로드 수정
    
    * fix: MultipartFile 어노테이션 변경
    
    * fix: Habit 프로필 imageUrl db에 저장 로직 추가
    
    * fix: MultipartFile 어노테이션 변경
    
    * feat: User의 WeeklyLog 작성 API 추가
    
    * fix: 습관 수정 예외처리 변경

--- src/main/java/ourtine/web/dto/request/GoalChangeRequestDto.java
+++ src/main/java/ourtine/web/dto/request/GoalChangeRequestDto.java
@@ -15,9 +15,8 @@ import javax.validation.constraints.Pattern;
 @AllArgsConstructor
 public class GoalChangeRequestDto {
 
-    @NotBlank
+    @NotBlank(message = "10~50 글자 사이로 입력해주세요.")
     @Length(min = 10, max = 50, message = "10~50 글자 사이로 입력해주세요.")
-    @Pattern(regexp="^[ㄱ-ㅎ가-힣a-z0-9-]*")
     private String goal;
 
 }

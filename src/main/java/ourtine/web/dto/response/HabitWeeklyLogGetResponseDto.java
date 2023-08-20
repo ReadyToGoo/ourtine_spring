e8e8b9084febc85b20a7a8b78bd542f3f736cc0e
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2023-08-20 22:34:01 +0900

    feat: 위클리 로그 로직 수정

--- src/main/java/ourtine/web/dto/response/HabitWeeklyLogGetResponseDto.java
+++ src/main/java/ourtine/web/dto/response/HabitWeeklyLogGetResponseDto.java
@@ -6,12 +6,16 @@ import lombok.NoArgsConstructor;
 import ourtine.domain.enums.Day;
 import ourtine.domain.enums.Emotion;
 
+import java.util.Date;
+
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class HabitWeeklyLogGetResponseDto {
     private Day day;
+    private Date date;
+    private String title;
     private String videoUrl;
     private Emotion emotion;
 }
